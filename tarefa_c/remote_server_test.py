dd23a76f6264a75c9bb7230478589fb99192a098
samuel-cavalcanti <scavalcanti111@gmail.com>
2020-10-15 22:13:43 -0300

    reaproveitando o código do testes

--- tarefa_c/remote_server_test.py
+++ tarefa_c/remote_server_test.py
@@ -6,6 +6,11 @@ from .remote_server import RemoteServer
 class RemoteServerTest(unittest.TestCase):
     __remote_server = RemoteServer()
 
+    def remove_file(self, file_name: str):
+        self.__remote_server.execute(f'rm {file_name}'.encode())
+        output_message = self.__remote_server.execute('ls'.encode())
+        self.assertFalse(file_name in output_message)
+
     def test_request_have_no_command(self):
         message = ''.encode()
         output_message = self.__remote_server.execute(message)
@@ -31,18 +36,16 @@ class RemoteServerTest(unittest.TestCase):
         message = f'touch {file_name}'.encode()
         self.__remote_server.execute(message)
         output_message = self.__remote_server.execute('ls'.encode())
-        self.__remote_server.execute(f'rm {file_name}'.encode())
         self.assertTrue(file_name in output_message)
-        output_message = self.__remote_server.execute('ls'.encode())
-        self.assertFalse(file_name in output_message)
+        self.remove_file(file_name)
 
     def test_create_new_file_with_content(self):
         file_name = 'new_file.txt'
         message = f'echo "content in new file" > {file_name}'.encode()
-        print(self.__remote_server.execute(message))
+        self.__remote_server.execute(message)
         output_message = self.__remote_server.execute(f'cat {file_name}'.encode())
-        self.__remote_server.execute(f'rm {file_name}'.encode())
         self.assertTrue('content in new file' in output_message)
+        self.remove_file(file_name)
 
 
 if __name__ == '__main__':
